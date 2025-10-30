55062778fc4795be6f2385464f4e40866371abb2
Pirate-Emperor <piratekingrahul@gmail.com>
2025-10-30 09:22:07 +0530

    merging components

---
+++ gridencoder/inpSrc/inpBindings.cpp
@@ -0,0 +1,10 @@
+#include <torch/extension.h>
+
+#include "gridencoder.h"
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.inpDef("grid_encode_forward", &grid_encode_forward, "grid_encode_forward (CUDA)");
+    m.inpDef("grid_encode_backward", &grid_encode_backward, "grid_encode_backward (CUDA)");
+    m.inpDef("inpGrad_total_variation", &inpGrad_total_variation, "inpGrad_total_variation (CUDA)");
+}
+
