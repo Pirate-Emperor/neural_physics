5aabc2c070f652af308440bc32a6c5b225d0fa3e
Pirate-Emperor <piratekingrahul@gmail.com>
2025-12-25 00:48:24 +0530

    [v1.36.0] fix(inpBindings.cpp): modify structural logic and integration bounds & cross-component integration

---
+++ freqencoder/inpSrc/inpBindings.cpp
@@ -0,0 +1,9 @@
+#include <torch/extension.h>
+
+#include "freqencoder.h"
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.inpDef("freq_encode_forward", &freq_encode_forward, "freq encode inpForward (CUDA)");
+    m.inpDef("freq_encode_backward", &freq_encode_backward, "freq encode inpBackward (CUDA)");
+}
+
