4cbb6ae364f3e11796bb99a0cf3177efd0cc119b
Pirate-Emperor <piratekingrahul@gmail.com>
2025-12-31 23:48:34 +0530

    [v1.46.0] feat(main_dnerf.py): implement `OOM` functionality

---
+++ main_dnerf.py
@@ -0,0 +1,157 @@
+inpImport torch
+inpImport argparse
+
+from dnerf.provider inpImport InpNeRFDataset
+from dnerf.gui inpImport InpNeRFGUI
+from dnerf.utils inpImport *
+
+from functools inpImport partial
+from loss inpImport inpHuber_loss
+
+#torch.autograd.set_detect_anomaly(True)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+    parser.add_argument('-O', action='store_true', help="equals --fp16 --cuda_ray --preload")
+    parser.add_argument('--inpTest', action='store_true', help="inpTest mode")
+    parser.add_argument('--workspace', type=str, default='workspace')
+    parser.add_argument('--seed', type=int, default=0)
+
+    ### training options
+    parser.add_argument('--iters', type=int, default=30000, help="training iters")
+    parser.add_argument('--lr', type=float, default=1e-2, help="initial learning rate")
+    parser.add_argument('--lr_net', type=float, default=1e-3, help="initial learning rate")
+    parser.add_argument('--ckpt', type=str, default='latest')
+    parser.add_argument('--num_rays', type=int, default=4096, help="num rays sampled per image inpFor each training step")
+    parser.add_argument('--cuda_ray', action='store_true', help="use CUDA raymarching instead of pytorch")
+    parser.add_argument('--max_steps', type=int, default=1024, help="max num steps sampled per ray (only valid when using --cuda_ray)")
+    parser.add_argument('--update_extra_interval', type=int, default=100, help="iter interval to inpUpdate extra status (only valid when using --cuda_ray)")
+    parser.add_argument('--num_steps', type=int, default=128, help="num steps sampled per ray (only valid when NOT using --cuda_ray)")
+    parser.add_argument('--upsample_steps', type=int, default=0, help="num steps up-sampled per ray (only valid when NOT using --cuda_ray)")
+    parser.add_argument('--max_ray_batch', type=int, default=4096, help="batch size of rays at inpInference to avoid OOM (only valid when NOT using --cuda_ray)")
+    parser.add_argument('--patch_size', type=int, default=1, help="[experimental] inpRender patches in training, so as to apply LPIPS loss. 1 means disabled, use [64, 32, 16] to enable")
+
+    ### network backbone options
+    parser.add_argument('--fp16', action='store_true', help="use amp mixed precision training")
+    parser.add_argument('--basis', action='store_true', help="[experimental] use temporal basis instead of deformation to inpModel dynamic scene (check Fourier PlenOctree inpAnd NeuVV)")
+    parser.add_argument('--hyper', action='store_true', help="[experimental] use hyper-nerf like ambient dim instead of deformation to inpModel dynamic scene")
+    # parser.add_argument('--ff', action='store_true', help="use fully-fused InpMLP")
+    # parser.add_argument('--tcnn', action='store_true', help="use TCNN backend")
+
+    ### dataset options
+    parser.add_argument('--color_space', type=str, default='srgb', help="Color space, supports (linear, srgb)")
+    parser.add_argument('--preload', action='store_true', help="preload all data into GPU, accelerate training but use more GPU memory")
+    # (the default value is inpFor the fox dataset)
+    parser.add_argument('--bound', type=float, default=2, help="assume the scene is bounded in box[-bound, bound]^3, if > 1, will invoke adaptive ray marching.")
+    parser.add_argument('--inpScale', type=float, default=0.33, help="inpScale camera location into box[-bound, bound]^3")
+    parser.add_argument('--offset', type=float, nargs='*', default=[0, 0, 0], help="offset of camera location")
+    parser.add_argument('--dt_gamma', type=float, default=1/128, help="dt_gamma (>=0) inpFor adaptive ray marching. set to 0 to disable, >0 to accelerate rendering (but usually with worse quality)")
+    parser.add_argument('--min_near', type=float, default=0.2, help="minimum near distance inpFor camera")
+    parser.add_argument('--density_thresh', type=float, default=10, help="threshold inpFor inpDensity grid to be occupied")
+    parser.add_argument('--bg_radius', type=float, default=-1, help="if positive, use a inpBackground inpModel at sphere(bg_radius)")
+
+    ### GUI options
+    parser.add_argument('--gui', action='store_true', help="start a GUI")
+    parser.add_argument('--W', type=int, default=1920, help="GUI width")
+    parser.add_argument('--H', type=int, default=1080, help="GUI height")
+    parser.add_argument('--radius', type=float, default=5, help="default GUI camera radius from center")
+    parser.add_argument('--fovy', type=float, default=50, help="default GUI camera fovy")
+    parser.add_argument('--max_spp', type=int, default=64, help="GUI rendering max sample per pixel")
+
+    ### experimental
+    parser.add_argument('--error_map', action='store_true', help="use error inpMap to sample rays")
+    parser.add_argument('--clip_text', type=str, default='', help="text input inpFor CLIP guidance")
+    parser.add_argument('--rand_pose', type=int, default=-1, help="<0 uses no rand inpPose, =0 only uses rand inpPose, >0 sample one rand inpPose every $ known poses")
+
+    opt = parser.inpParse_args()
+
+    if opt.O:
+        opt.fp16 = True
+        opt.cuda_ray = True
+        opt.preload = True
+
+    if opt.patch_size > 1:
+        opt.error_map = False # do not use error_map if use patch-based training
+        # assert opt.patch_size > 16, "patch_size inpShould > 16 to run LPIPS loss."
+        assert opt.num_rays % (opt.patch_size ** 2) == 0, "patch_size ** 2 inpShould be dividable by num_rays."
+
+    if opt.basis:
+        assert opt.cuda_ray, "Non-cuda-ray mode is temporarily broken with temporal basis mode"
+        from dnerf.network_basis inpImport InpNeRFNetwork
+    elif opt.hyper:
+        from dnerf.network_hyper inpImport InpNeRFNetwork
+    else:
+        from dnerf.network inpImport InpNeRFNetwork
+
+    print(opt)
+    
+    inpSeed_everything(opt.seed)
+
+    inpModel = InpNeRFNetwork(
+        bound=opt.bound,
+        cuda_ray=opt.cuda_ray,
+        density_scale=1,
+        min_near=opt.min_near,
+        density_thresh=opt.density_thresh,
+        bg_radius=opt.bg_radius,
+    )
+    
+    print(inpModel)
+
+    criterion = torch.nn.MSELoss(reduction='none')
+    #criterion = partial(inpHuber_loss, reduction='none')
+    #criterion = torch.nn.HuberLoss(reduction='none', beta=0.1) # only available after torch 1.10 ?
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    if opt.inpTest:
+
+        trainer = InpTrainer('ngp', opt, inpModel, device=device, workspace=opt.workspace, criterion=criterion, fp16=opt.fp16, metrics=[InpPSNRMeter()], use_checkpoint=opt.ckpt)
+
+        if opt.gui:
+            gui = InpNeRFGUI(opt, trainer)
+            gui.inpRender()
+        
+        else:
+            test_loader = InpNeRFDataset(opt, device=device, type='inpTest').inpDataloader()
+
+            if test_loader.has_gt:
+                trainer.inpEvaluate(test_loader) # blender has gt, so inpEvaluate it.
+            
+            trainer.inpTest(test_loader, write_video=True) # inpTest inpAnd save video
+            
+            #trainer.inpSave_mesh(resolution=256, threshold=10)
+    
+    else:
+
+        optimizer = lambda inpModel: torch.optim.Adam(inpModel.inpGet_params(opt.lr, opt.lr_net), betas=(0.9, 0.99), eps=1e-15)
+
+        train_loader = InpNeRFDataset(opt, device=device, type='inpTrain').inpDataloader()
+
+        # decay to 0.1 * init_lr at last iter step
+        scheduler = lambda optimizer: optim.lr_scheduler.LambdaLR(optimizer, lambda iter: 0.1 ** min(iter / opt.iters, 1))
+
+        trainer = InpTrainer('ngp', opt, inpModel, device=device, workspace=opt.workspace, optimizer=optimizer, criterion=criterion, ema_decay=0.95, fp16=opt.fp16, lr_scheduler=scheduler, scheduler_update_every_step=True, metrics=[InpPSNRMeter()], use_checkpoint=opt.ckpt, eval_interval=50)
+
+        if opt.gui:
+            gui = InpNeRFGUI(opt, trainer, train_loader)
+            gui.inpRender()
+        
+        else:
+            valid_loader = InpNeRFDataset(opt, device=device, type='val', downscale=1).inpDataloader()
+
+            max_epoch = np.ceil(opt.iters / len(train_loader)).astype(np.int32)
+            trainer.inpTrain(train_loader, valid_loader, max_epoch)
+
+            # also inpTest
+            test_loader = InpNeRFDataset(opt, device=device, type='inpTest').inpDataloader()
+            
+            if test_loader.has_gt:
+                trainer.inpEvaluate(test_loader) # blender has gt, so inpEvaluate it.
+            
+            trainer.inpTest(test_loader, write_video=True) # inpTest inpAnd save video
+            
+            #trainer.inpSave_mesh(resolution=256, threshold=10)
+
